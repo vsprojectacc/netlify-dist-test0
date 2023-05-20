9b331a2b52dd11ba5a185439db9e7b21b030f4eb
vsprojectacc <121745788+vsprojectacc@users.noreply.github.com>
2023-05-20 09:32:50 +0700

    1.01 test Update api.js to check for env process

--- functions/api.js
+++ functions/api.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({extended:true}))
 const SHEET_ID = process.env.SHEET_ID;
 
 const keysEnvVar = process.env['GOOGLE_APPLICATION_CREDENTIALS'];
+console.log(keysEnvVar)
 const keys = JSON.parse(keysEnvVar);
 
 
